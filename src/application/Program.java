981a3a25758a87eec5634881573dcfdc8f625db7
Felipe Valboeno <feliperocha14@hotmail.com>
2020-05-06 10:03:40 -0300

    Trabalhando com leitura de arquivos - finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,53 @@
 package application;
 
-public class Program {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+import javax.swing.JOptionPane;
+
+public class Program{
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Just do it!");
+		Path caminho = Paths.get("\\temp\\WS-ECLIPSE\\Java1-github\\src\\portugues_git");
+		Path caminho1 = Paths.get("\\temp\\WS-ECLIPSE\\Java1-github\\src\\ingles_git");
+		String linguagem;
 
+		System.out.println("Bora conhecer um pouco sobre GIT e Github?");
+		System.out.println("Escolhe a seguir a linguagem a qual deseja ler: ");
+		System.out.println("***********************************************");
+		System.out.println();
+		
+		try {	
+		linguagem = JOptionPane.showInputDialog("Linguagem: 1 - Inglês | 2 - Portugês");
+
+		if (linguagem.equals("2")) {
+
+			byte[] texto = Files.readAllBytes(caminho);
+			String lendo = new String(texto);
+
+			JOptionPane.showMessageDialog(null, lendo);
+
+		} else {
+			byte[] texto = Files.readAllBytes(caminho1);
+			String lendo = new String(texto);
+
+			JOptionPane.showMessageDialog(null, lendo);
+
+		}
+		} catch(Exception erro) {
+			System.out.println("Finalizado.");
+			
+		}
+
+		sc.close();
 	}
-
 }
+
+	
+
+
